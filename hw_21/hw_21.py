9b09297dc332a4bd280d2a80adf5a48e2c5cafe0
Grigory < >
2024-11-23 14:55:35 +0500

    Добавил условие для игрока

--- hw_21/hw_21.py
+++ hw_21/hw_21.py
@@ -3,13 +3,10 @@ from cities import cities_list as cl
 
 city_set = set()
 
-# for city in cl:
-#     city_set.add((city["name"]))
-
 city_set = {city['name'] for city in cl}
 
-
-
+last_city = ''
+bad_letters = {'ь', 'ъ', 'ы', 'й'}
 
 while True:
     print("Для выхода из игры напишите слово 'стоп'")
@@ -19,17 +16,25 @@ while True:
         print("Вы проиграли!")
         break
 
+    if last_city:
+        last_letter = last_city[-1]
+
+        if last_letter in bad_letters:
+            last_letter = last_city[-2]
+
+        if user_input[0].lower() != last_letter:
+            print(f'Город должен начинаться на букву "{last_letter.upper()}"')
+            continue
+
     if user_input not in city_set:
         print("Такого города не существует, либо он уже использован")
         continue
-
 
     city_set.remove(user_input)
     print("Ваш город", user_input)
     
     result = []
     for city in city_set:
-        bad_letters = {'ь', 'ъ', 'ы', 'й'}
 
         if user_input[-1] in bad_letters:
             if city[0].lower() == user_input[-2]:
@@ -44,5 +49,6 @@ while True:
 
     comp_city = choice(result)
     city_set.remove(comp_city)
+    last_city = comp_city
     print("Город соперника:", comp_city)
 
