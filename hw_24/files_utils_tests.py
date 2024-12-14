e3856cee73c02ec55833e70f03750208c1c02d69
PimpCoach <->
2024-12-14 13:35:04 +0500

    Протестировали функцию на добавление данных в словарь

--- hw_24/files_utils_tests.py
+++ hw_24/files_utils_tests.py
@@ -5,23 +5,34 @@ from files_utils import read_json, write_json, append_json
 # read_JSON = read_json("hw_24/test.json")
 # print(read_JSON)
 
-#Перезаписываем
-write = {
-    "name": "Иван Петров",
-    "age": 25,
-    "isStudent": True,
-    "hobbies": [
-        "программирование",
-        "футбол",
-        "чтение"
-    ],
-    "address": {
-        "street": "ул. Ленина",
-        "house": 12,
-        "city": "Москва",
-        "zipCode": "123456"
-    }
+# #Перезаписываем
+# write = {
+#     "name": "Иван Петров",
+#     "age": 25,
+#     "isStudent": True,
+#     "hobbies": [
+#         "программирование",
+#         "футбол",
+#         "чтение"
+#     ],
+#     "address": {
+#         "street": "ул. Ленина",
+#         "house": 12,
+#         "city": "Москва",
+#         "zipCode": "123456"
+#     }
+# }
+# write_json(write, "hw_24/test.json")
+
+#Добавляем
+apend = {
+    "fiction": {
+        "fantasy": ["Гарри Поттер", "Властелин колец"],
+        "detective": ["Шерлок Холмс", "Пуаро"],
+    },
+    "non_fiction": {
+        "science": ["Краткая история времени"],
+        "biography": ["Стив Джобс"],
+    },
 }
-write_json(write, "hw_24/test.json")
-
-
+append_json(apend, "hw_24/test.json")
