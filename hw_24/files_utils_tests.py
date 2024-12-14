74580acee87e0eea92ca116fc3381fba000fb233
PimpCoach <->
2024-12-14 13:33:19 +0500

    Протестировали функцию на запись

--- hw_24/files_utils_tests.py
+++ hw_24/files_utils_tests.py
@@ -1,7 +1,27 @@
 # Тестирования JSON
 from files_utils import read_json, write_json, append_json
 
-#Читаем
-read_JSON = read_json("hw_24/test.json")
-print(read_JSON)
+# #Читаем
+# read_JSON = read_json("hw_24/test.json")
+# print(read_JSON)
+
+#Перезаписываем
+write = {
+    "name": "Иван Петров",
+    "age": 25,
+    "isStudent": True,
+    "hobbies": [
+        "программирование",
+        "футбол",
+        "чтение"
+    ],
+    "address": {
+        "street": "ул. Ленина",
+        "house": 12,
+        "city": "Москва",
+        "zipCode": "123456"
+    }
+}
+write_json(write, "hw_24/test.json")
+
 
