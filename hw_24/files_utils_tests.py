dafd0ec4dbb90034cd6c3c0086a9572fe80b0c21
PimpCoach <->
2024-12-15 16:53:21 +0500

    тестируем запись в CSV

--- hw_24/files_utils_tests.py
+++ hw_24/files_utils_tests.py
@@ -1,12 +1,18 @@
 # Тестирования CSV
 from files_utils import read_csv, write_csv, append_csv
 
-#Читаем
-read = read_csv('hw_24/test_csv.csv')
-print(read)
+# #Читаем
+# read = read_csv('hw_24/test_csv.csv')
+# print(read)
 
 #Перезаписываем
 
+new_student_list = [
+    "Анатолий",
+    "Андреевич",
+    "Андреев",
+]
+write = write_csv(new_student_list, 'hw_24/test_csv.csv')
 
 #Добавляем
 
