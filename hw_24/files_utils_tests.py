3c98bf667ee3dccb17487993504ae5b2971cd803
PimpCoach <->
2024-12-15 16:55:09 +0500

    тестируем дабавление в CSV

--- hw_24/files_utils_tests.py
+++ hw_24/files_utils_tests.py
@@ -7,12 +7,19 @@ from files_utils import read_csv, write_csv, append_csv
 
 #Перезаписываем
 
-new_student_list = [
-    "Анатолий",
-    "Андреевич",
-    "Андреев",
-]
-write = write_csv(new_student_list, 'hw_24/test_csv.csv')
+# new_student_list = [
+#     "Анатолий",
+#     "Андреевич",
+#     "Андреев",
+# ]
+# write = write_csv(new_student_list, 'hw_24/test_csv.csv')
 
 #Добавляем
 
+new_student_list = [
+    "Василий",
+    "Васильевич",
+    "Васильев",
+]
+
+append = append_csv(new_student_list, 'hw_24/test_csv.csv')
