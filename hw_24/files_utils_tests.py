e99258b7b550b48baa2153fad86e0866e791893e
PimpCoach <->
2024-12-15 16:51:59 +0500

    Тестируем чтение CSV

--- hw_24/files_utils_tests.py
+++ hw_24/files_utils_tests.py
@@ -1,38 +1,12 @@
-# Тестирования JSON
-from files_utils import read_json, write_json, append_json
+# Тестирования CSV
+from files_utils import read_csv, write_csv, append_csv
 
-# #Читаем
-# read_JSON = read_json("hw_24/test.json")
-# print(read_JSON)
+#Читаем
+read = read_csv('hw_24/test_csv.csv')
+print(read)
 
-# #Перезаписываем
-# write = {
-#     "name": "Иван Петров",
-#     "age": 25,
-#     "isStudent": True,
-#     "hobbies": [
-#         "программирование",
-#         "футбол",
-#         "чтение"
-#     ],
-#     "address": {
-#         "street": "ул. Ленина",
-#         "house": 12,
-#         "city": "Москва",
-#         "zipCode": "123456"
-#     }
-# }
-# write_json(write, "hw_24/test.json")
+#Перезаписываем
+
 
 #Добавляем
-apend = {
-    "fiction": {
-        "fantasy": ["Гарри Поттер", "Властелин колец"],
-        "detective": ["Шерлок Холмс", "Пуаро"],
-    },
-    "non_fiction": {
-        "science": ["Краткая история времени"],
-        "biography": ["Стив Джобс"],
-    },
-}
-append_json(apend, "hw_24/test.json")
+
