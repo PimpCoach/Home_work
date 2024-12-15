c1d7a6a83e1be77ae672310b72a99debea67e760
PimpCoach <->
2024-12-15 19:06:46 +0500

    тестируем yaml

--- hw_24/files_utils_tests.py
+++ hw_24/files_utils_tests.py
@@ -1,14 +1,6 @@
 # Тестирования TXT
-from files_utils import read_txt, write_txt, append_txt
+from files_utils import read_yaml
 
-# # Читаем
-# read = read_txt("hw_24/test_txt.txt")
-# print(read)
-
-# # Записываем
-# text = "Тест записи"
-# write = write_txt(text, "hw_24/test_txt.txt")
-
-# Добавляем
-new_text = "Добавляем запись"
-append = append_txt(new_text, "hw_24/test_txt.txt")
+# Читаем
+read = read_yaml("hw_24/test_yaml.yaml")
+print(read)
