cd177ac0790d89ec980407aea06f2a2fbba1eec9
PimpCoach <->
2024-12-15 17:36:03 +0500

    Тестируем добавление записи

--- hw_24/files_utils_tests.py
+++ hw_24/files_utils_tests.py
@@ -5,6 +5,10 @@ from files_utils import read_txt, write_txt, append_txt
 # read = read_txt("hw_24/test_txt.txt")
 # print(read)
 
-# Записываем
-text = "Тест записи"
-write = write_txt(text, "hw_24/test_txt.txt")
+# # Записываем
+# text = "Тест записи"
+# write = write_txt(text, "hw_24/test_txt.txt")
+
+# Добавляем
+new_text = "Добавляем запись"
+append = append_txt(new_text, "hw_24/test_txt.txt")
