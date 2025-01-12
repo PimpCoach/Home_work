53d534d40f22e9490dcc8ffc3d1f8b278d38f31f
PimpCoach <->
2025-01-12 22:29:21 +0500

    Из импортированного словаря создал словарь, который имеет в себе данные тех "id", которые ввел пользователь

--- hw_25/hw_25.py
+++ hw_25/hw_25.py
@@ -2,3 +2,6 @@ from marvel import full_dict as fd
 
 user_num = list(map(int, input('Введите цифры через пробел:').split()))
 print(user_num)
+
+DictForUserNum = dict(filter(lambda id: id[0] in user_num, fd.items()))
+print(DictForUserNum)
