e9f650e836cb2cb4b86892d44e199d0a5c43fef7
PimpCoach <->
2025-01-12 22:48:24 +0500

    hw_25. Исправил нэйминг, создал уникальные значения ключа 'director' с помощью `set comprehension`

--- hw_25/hw_25.py
+++ hw_25/hw_25.py
@@ -1,7 +1,10 @@
 from marvel import full_dict as fd
 
-user_num = list(map(int, input('Введите цифры через пробел:').split()))
-print(user_num)
+# user_num = list(map(int, input('Введите цифры через пробел:').split()))
+# print(user_num)
 
-DictForUserNum = dict(filter(lambda id: id[0] in user_num, fd.items()))
-print(DictForUserNum)
+# dict_for_user_num = dict(filter(lambda id: id[0] in user_num, fd.items()))
+# print(dict_for_user_num)
+
+director_movies = {movie[1]['director'] for movie in fd.items()}
+print(director_movies)
