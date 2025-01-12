ab363e118bacedb86d4be740222f1b36bfe1178e
PimpCoach <->
2025-01-12 23:53:31 +0500

    hw_25. Создал словарь с фильмами которые начинаются на "Ч" с помощью "filter"

--- hw_25/hw_25.py
+++ hw_25/hw_25.py
@@ -6,8 +6,12 @@ from marvel import full_dict as fd
 # dict_for_user_num = dict(filter(lambda id: id[0] in user_num, fd.items()))
 # print(dict_for_user_num)
 
-director_movies = {movie[1]['director'] for movie in fd.items()}
-print(director_movies)
+# director_movies = {movie[1]['director'] for movie in fd.items()}
+# print(director_movies)
 
-full_dict_copy = {key: {**full_dict, 'year': str(full_dict['year'])} for key, full_dict in fd.items()}
-print(full_dict_copy)
+# full_dict_copy = {key: {**full_dict, 'year': str(full_dict['year'])} for key, full_dict in fd.items()}
+# print(full_dict_copy)
+
+dict_for_letter = dict(filter(lambda film: film[1]['title'] and film[1]['title'][0] == "Ч", fd.items()))
+print(dict_for_letter)
+
