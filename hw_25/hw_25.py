8c4e6ef61dd084a3a7f04a0eddc9e900b1792a87
PimpCoach <->
2025-01-13 00:22:33 +0500

    Импортировал 'pprint' и добавил его в 3, 5, 6, 7 и 8 задание

--- hw_25/hw_25.py
+++ hw_25/hw_25.py
@@ -1,24 +1,26 @@
+from pprint import pprint
 from marvel import full_dict as fd
+
 
 # user_num = list(map(int, input('Введите цифры через пробел:').split()))
 # print(user_num)
 
 # dict_for_user_num = dict(filter(lambda id: id[0] in user_num, fd.items()))
-# print(dict_for_user_num)
+# pprint(dict_for_user_num)
 
 # director_movies = {movie[1]['director'] for movie in fd.items()}
 # print(director_movies)
 
 # full_dict_copy = {key: {**full_dict, 'year': str(full_dict['year'])} for key, full_dict in fd.items()}
-# print(full_dict_copy)
+# pprint(full_dict_copy)
 
 # dict_for_letter = dict(filter(lambda film: film[1]['title'] and film[1]['title'][0] == "Ч", fd.items()))
-# print(dict_for_letter)
+# pprint(dict_for_letter)
 
 # sorted_full_dict = sorted(fd.items(), key=lambda film: film[1]["director"])
-# print(sorted_full_dict)
+# pprint(sorted_full_dict)
 
-sorted_full_dict_2 = sorted(
-    fd.items(), key=lambda film: (film[1]["director"], film[1]["title"] or "")
-)
-print(sorted_full_dict_2)
+# sorted_full_dict_2 = sorted(
+#     fd.items(), key=lambda film: (film[1]["director"], film[1]["title"] or "")
+# )
+# pprint(sorted_full_dict_2)
