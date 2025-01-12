06552cc5bd17e16b6c733b07641fc53a5a1537d5
PimpCoach <->
2025-01-13 01:14:02 +0500

    Импортировал модуль 'typing' для аннотации типов. Установил пакет "MyPy" прошел с помощью него проверку на правильность аннотаций типов.

--- hw_25/hw_25.py
+++ hw_25/hw_25.py
@@ -1,29 +1,45 @@
 from pprint import pprint
+
+from typing import List, Dict, Set, Any, Tuple
+
 from marvel import full_dict as fd
 
-
-# user_num = list(map(int, input('Введите цифры через пробел:').split()))
+# user_num = list(map(int, input("Введите цифры через пробел:").split()))
 # print(user_num)
 
 # dict_for_user_num = dict(filter(lambda id: id[0] in user_num, fd.items()))
 # pprint(dict_for_user_num)
 
-# director_movies = {movie[1]['director'] for movie in fd.items()}
+# director_movies: Set[str] = {movie[1]["director"] for movie in fd.items()}
 # print(director_movies)
 
-# full_dict_copy = {key: {**full_dict, 'year': str(full_dict['year'])} for key, full_dict in fd.items()}
+# full_dict_copy: Dict[int, Dict[str, Any]] = {
+#     key: {**full_dict, "year": str(full_dict["year"])} for key, full_dict in fd.items()
+# }
+
 # pprint(full_dict_copy)
 
-# dict_for_letter = dict(filter(lambda film: film[1]['title'] and film[1]['title'][0] == "Ч", fd.items()))
+# dict_for_letter = dict(
+#     filter(lambda film: film[1]["title"] and film[1]["title"][0] == "Ч", fd.items())
+# )
 # pprint(dict_for_letter)
 
-# sorted_full_dict = sorted(fd.items(), key=lambda film: film[1]["director"])
-# print(sorted_full_dict)
+# sorted_full_dict: List[Tuple[int, Dict[str, Any]]] = sorted(
+#     fd.items(), key=lambda film: film[1]["director"]
+# )
+# pprint(sorted_full_dict)
 
-# sorted_full_dict_2 = sorted(
+# sorted_full_dict_2: List[Tuple[int, Dict[str, Any]]] = sorted(
 #     fd.items(), key=lambda film: (film[1]["director"], film[1]["title"] or "")
 # )
 # pprint(sorted_full_dict_2)
 
-sorted_dict_for_letter = dict(sorted(filter(lambda film: film[1]['title'] and film[1]['title'][0] == "Ч", fd.items()), key=lambda film: film[1]["director"]))
+sorted_dict_for_letter = dict(
+    sorted(
+        filter(
+            lambda film: film[1]["title"] and film[1]["title"][0] == "Ч", fd.items()
+        ),
+        key=lambda film: film[1]["director"],
+    )
+)
 pprint(sorted_dict_for_letter, sort_dicts=False)
