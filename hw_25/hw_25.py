de58ad71b4ff9613aa44792d6e81461507324dc3
PimpCoach <->
2025-01-13 00:03:23 +0500

    hw_25. Остортировал исходный словарь по значению 'director'

--- hw_25/hw_25.py
+++ hw_25/hw_25.py
@@ -12,6 +12,9 @@ from marvel import full_dict as fd
 # full_dict_copy = {key: {**full_dict, 'year': str(full_dict['year'])} for key, full_dict in fd.items()}
 # print(full_dict_copy)
 
-dict_for_letter = dict(filter(lambda film: film[1]['title'] and film[1]['title'][0] == "Ч", fd.items()))
-print(dict_for_letter)
+# dict_for_letter = dict(filter(lambda film: film[1]['title'] and film[1]['title'][0] == "Ч", fd.items()))
+# print(dict_for_letter)
+
+sorted_full_dict = sorted(fd.items(), key=lambda film: film[1]['director'])
+print(sorted_full_dict)
 
