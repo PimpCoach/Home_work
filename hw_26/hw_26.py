cd217b4859df0072e44193b000f2dc6238f5971c
PimpCoach <->
2025-01-20 22:51:47 +0500

    hw_26. Часть 1: Декоратор для валидации пароля с примерами использованиями

---
+++ hw_26/hw_26.py
@@ -0,0 +1,40 @@
+import re
+from typing import Callable, Any 
+
+
+def password_checker(func: Callable):
+    def wrapper(args: Any):
+        
+        # Минимальная длина
+        if len(args) < 8:
+            return "Пароль должен содержать как минимум 8 символов"
+
+        # Наличие цифры
+        if not re.search(r'[0,1,2,3,4,5,6,7,8,9]', args):
+            return "Пароль должен собержать как минимум 1 цифру"
+        
+        # Наличие заглавной буквы
+        if not re.search(r"[A-Z]", args):
+            return "Пароль должен содержать как минимум 1 заглавную букву"
+        
+        # Наличие строчной буквы
+        if not re.search(r'[a-z]', args):
+            return 'Пароль должен содержать как минимум 1 строчную букву'
+        
+        # Наличие спец символа
+        if not re.search(r'[!, @, #, $, /, ?, |]', args):
+            return 'Паролько должен содержать как минимум 1 спец символ !, @, #, $, /, ?, |'
+        
+        return func(args)
+    
+    return wrapper
+
+@password_checker
+def register_user(password):
+    return f"Пароль {password} прошел проверку"
+
+print(register_user('qwerty'))
+print(register_user('qwerty123'))
+print(register_user('Qwerty123'))
+print(register_user('Qwerty123!'))
+
