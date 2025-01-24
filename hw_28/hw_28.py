f4ae0b9058f234103bc7df7e984032df8d6f82fc
PimpCoach <->
2025-01-24 14:15:51 +0500

    hw_28. Исправил аннотацию типов, добавил документацию к классу и каждому методу.

--- hw_28/hw_28.py
+++ hw_28/hw_28.py
@@ -1,19 +1,62 @@
 # Класс для работы с текстовыми файлами
 class TxtFileHandler:
-    # Метод чтения 
+    """
+    Класс для работы с текстовыми файлами с методами чтения, записи 
+    и добавления данных в текстовый документ с обработкой исключений
+    """
+    
     @staticmethod
     def read_file(file_path: str, encoding: str = 'utf-8') -> str:
-            with open(file_path, 'r', encoding=encoding) as file:
-                return file.read()
-        
-    # Метод записи 
-    @staticmethod
-    def write_file(file_path: str, *data: str, encoding: str = 'utf-8') -> None:
-        with open(file_path, 'w', encoding=encoding) as file:
-            file.write(*data + '\n')
+            """
+            Чтение TXT файла 
+            :param file_path: путь к файлу в виде строки
+            :paran encoding: кодировка файла по умолчанию 'utf-8)
+            :return: содержимое файла в виде строки,
+                    при ошибке пустую строку
+            """
+            try:
+                with open(file_path, 'r', encoding=encoding) as file:
+                    return file.read()
+            except FileNotFoundError:
+                print(f'Файла {file_path} не существует')
+                return ""
+            except PermissionError:
+                print(f'Ошибка доступа при чтении файла {file_path}')
+                return ""
 
-    #Метод добавления данных 
     @staticmethod
-    def append_file(file_path: str, *data: str, encoding: str = 'utf-8') -> None:
-        with open(file_path, 'a', encoding=encoding) as file:
-            file.write(*data + '\n')
+    def write_file(file_path: str, *data: str, encoding: str = 'utf-8') -> bool:
+        """
+        Запись данных в файл с обрабоской ошибок
+        :param file_path: путь к файлу в виде строки
+        :param *data
+        :paran encoding: кодировка файла по умолчанию 'utf-8)
+        :return: True если запись успешна,
+                False если ошибка)
+        """
+        try:
+            with open(file_path, 'w', encoding=encoding) as file:
+                file.write(*data)
+                return True
+        except PermissionError:
+                print(f'Ошибка доступа при чтении файла {file_path}')
+                return False
+
+    @staticmethod
+    def append_file(file_path: str, *data: str, encoding: str = 'utf-8') -> bool:
+        """
+        Добавление данных в файл с обрабоской ошибок
+        Запись данных в файл с обрабоской ошибок
+        :param file_path: путь к файлу в виде строки
+        :paran encoding: кодировка файла по умолчанию 'utf-8)
+        :return: True если запись успешна,
+                False если ошибка 
+        """
+        try:
+            with open(file_path, 'a', encoding=encoding) as file:
+                file.write(*data)
+                return True
+        except PermissionError:
+                print(f'Ошибка доступа при чтении файла {file_path}')
+                return False
+
